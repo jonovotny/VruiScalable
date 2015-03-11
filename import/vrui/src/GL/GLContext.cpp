e458a8687a89c93b88743e3d3c9f077f4d1cf9a9
Wesley Miller <wesdizzle@gmail.com>
2015-03-11 14:49:59 -0400

    changed assignment of POL file to be dependent on static array instead of computations with projector, node, and x server indices

--- import/vrui/src/GL/GLContext.cpp
+++ import/vrui/src/GL/GLContext.cpp
@@ -37,25 +37,20 @@ GLContext::GLContext(const char* displayName,int* visualProperties)
 	:display(0),visual(0),depth(-1),
 	 extensionManager(0),contextData(0)
 	{
+
 	/* Define POL filename */
-	int offset[] = {0, 1, 3, 2};
-	std::stringstream sstm;
-	int projNum;
-	
-	for(int nodeNum = 1; nodeNum <= 7; nodeNum++)
+
+	//array for each projector index holding a string identifying the node and x server
+	projectors[] = {"cave010:0.0", "cave010:0.1", "cave010:0.2", "cave009:0.0", "cave009:0.1", "cave009:0.2", "cave009:0.3", "cave008:0.0", "cave008:0.1", "cave008:0.2", "cave007:0.0", "cave007:0.1", "cave007:0.3", "cave007:0.2", "cave006:0.0", "cave006:0.1", "cave006:0.3", "cave006:0.2", "cave005:0.0", "cave005:0.1", "cave005:0.3", "cave005:0.2", "cave004:0.0", "cave004:0.1", "cave004:0.3", "cave004:0.2", "cave003:0.0", "cave003:0.1", "cave003:0.3", "cave003:0.2", "cave002:0.0", "cave002:0.1", "cave002:0.3", "cave002:0.2", "cave001:0.0", "cave001:0.1", "cave001:0.3", "cave001:0.2"};
+
+	for(int i = 0; i < 38; i++)
 	{
-		for(int xNum = 0; xNum < 4; xNum++)
+		if(projectors[i] == displayName)
 		{
-			sstm << "cave00" << nodeNum << ":0." << xNum;
-			if(displayName == sstm.str())
-			{
-				projNum = 38 - 4*(nodeNum) + offset[xNum];
-				sstm.str(std::string());
-				sstm << "/gpfs/home/cavedemo/scalable/cave/ScalableData.pol_" << projNum;
-				filename = sstm.str(); 
-			}
-			//clear stringstream
+			std::stringstream sstm;
 			sstm.str(std::string());
+			sstm << "/gpfs/home/cavedemo/scalable/cave/ScalableData.pol_" << projNum;
+			filename = sstm.str(); 
 		}
 	}
 
