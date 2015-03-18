cff4d86b036ca2b706175a5fcc1317ef56490262
Wesley Miller <wesdizzle@gmail.com>
2015-03-18 14:42:50 -0400

    updated if/else cases and necessary argument to allow for left and right buffers in stereo or a single buffer in mono

--- import/vrui/src/Vrui/Scalable.h
+++ import/vrui/src/Vrui/Scalable.h
@@ -4,7 +4,7 @@
 #define _EASYBLENDSDK_LINUX
 #include "EasyBlendSDK.h"
 
-EasyBlendSDK_Mesh * initScalableMesh(std::string displayName, bool left)
+EasyBlendSDK_Mesh * initScalableMesh(std::string displayName, std::string specifier)
 {
 	std::string projectors[] = {"cave010:0.0", "cave010:0.1", "cave010:0.2", "cave009:0.0", "cave009:0.1", "cave009:0.2", "cave009:0.3", "cave008:0.0", "cave008:0.1", "cave008:0.2", "cave007:0.0", "cave007:0.1", "cave007:0.3", "cave007:0.2", "cave006:0.0", "cave006:0.1", "cave006:0.3", "cave006:0.2", "cave005:0.0", "cave005:0.1", "cave005:0.3", "cave005:0.2", "cave004:0.0", "cave004:0.1", "cave004:0.3", "cave004:0.2", "cave003:0.0", "cave003:0.1", "cave003:0.3", "cave003:0.2", "cave002:0.0", "cave002:0.1", "cave002:0.3", "cave002:0.2", "cave001:0.0", "cave001:0.1", "cave001:0.3", "cave001:0.2"};
 
@@ -24,15 +24,20 @@ EasyBlendSDK_Mesh * initScalableMesh(std::string displayName, bool left)
 	EasyBlendSDK_Mesh *gMSDK = new EasyBlendSDK_Mesh;
 	EasyBlendSDK_Initialize(POLfileName.c_str(), gMSDK);
 
-	if(left)
+	if(specifier == "left")
 	{
 		EasyBlendSDK_SetInputReadBuffer(gMSDK, GL_BACK_LEFT);
 		EasyBlendSDK_SetOutputDrawBuffer(gMSDK,  GL_BACK_LEFT);
 	}
-	else
+	else if(specifier == "right")
 	{
 		EasyBlendSDK_SetInputReadBuffer(gMSDK, GL_BACK_RIGHT);
 		EasyBlendSDK_SetOutputDrawBuffer(gMSDK,  GL_BACK_RIGHT);
+	}
+	else
+	{
+		EasyBlendSDK_SetInputReadBuffer(gMSDK, GL_BACK);
+		EasyBlendSDK_SetOutputDrawBuffer(gMSDK,  GL_BACK);
 	}
 
 	return gMSDK;
