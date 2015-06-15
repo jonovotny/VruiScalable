6bb90ddc9ae6138cf9aa4a6dde61bcd129c27ba0
Wesley Miller <wesdizzle@gmail.com>
2015-06-15 14:18:14 -0400

    comment out dynamic eyepoint to work refine main Scalable portion

--- import/vrui/src/Vrui/VRWindow.h
+++ import/vrui/src/Vrui/VRWindow.h
@@ -35,7 +35,7 @@ Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
 
 #ifdef USE_SCALABLE
 #define _EASYBLENDSDK_LINUX
-#include "Scalable.h"
+//#include "Scalable.h"
 #include "EasyBlendSDK.h"
 #endif
 
