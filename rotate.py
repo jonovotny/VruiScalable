48e04dd45cc103cca3fbec82cf73b6f3dd824cc1
Wesley Miller <wesdizzle@gmail.com>
2015-10-30 18:02:51 -0400

    updated to rotate 180 degrees about z axis

--- rotate.py
+++ rotate.py
@@ -7,4 +7,10 @@ if os.path.isfile(sys.argv[2]):
 with open(sys.argv[1]) as f:
 	for line in f:
 		newfile = open(sys.argv[2], 'a')
-		newfile.write(line)
+		els = line.split(" ")
+		if els[0] == "v" or els[0] == "vn":
+			newfile.write(els[0] + " " + " ".join([str(-float(el)) for el in els[1:3]]) + " " + els[3])
+			if "\n" not in els[3]:
+				newfile.write("\n")
+		else:
+			newfile.write(line)
