b8c2543d95913b22f5e5ece32e75cc4e31b01eee
Wesley Miller <wesdizzle@gmail.com>
2016-04-24 19:22:46 -0400

    add lighting based on camera position

--- import/3dvisualizer/src/share/3DVisualizer-1.14/Shaders/SingleChannelRaycaster.fs
+++ import/3dvisualizer/src/share/3DVisualizer-1.14/Shaders/SingleChannelRaycaster.fs
@@ -31,6 +31,37 @@ uniform sampler1D colorMapSampler;
 varying vec3 mcPosition;
 varying vec3 dcPosition;
 
+/* Brown Specific */
+uniform int gridMode;
+uniform float gridSize;
+uniform float gridWidth;
+uniform int lightMode;
+
+
+vec3 blinn_phong(vec3 N, vec3 V, vec3 L, int light)
+{
+    // material properties
+    // you might want to put this into a bunch or uniforms
+    vec3 Ka = vec3(1.0, 1.0, 1.0);
+    vec3 Kd = vec3(1.0, 1.0, 1.0);
+    vec3 Ks = vec3(1.0, 1.0, 1.0);
+    float shininess = 50.0;
+
+    // diffuse coefficient
+    float diff_coeff = max(dot(L,N),0.0);
+
+    // specular coefficient
+    vec3 H = normalize(L+V);
+    float spec_coeff = pow(max(dot(H,N), 0.0), shininess);
+    if (diff_coeff <= 0.0) 
+        spec_coeff = 0.0;
+
+    // final lighting model
+    return  Ka * gl_LightSource[light].ambient.rgb + 
+            Kd * gl_LightSource[light].diffuse.rgb  * diff_coeff + 
+            Ks * gl_LightSource[light].specular.rgb * spec_coeff ;
+}
+
 void main()
 	{
 	/* Calculate the ray direction in model coordinates: */
@@ -61,6 +92,11 @@ void main()
 	/* Move the ray starting position forward to an integer multiple of the step size: */
 	vec3 samplePos=dcPosition;
 	float lambda=ceil(eyeDist)-eyeDist;
+	
+	vec3 sampleMult=samplePos;
+	vec3 sampleMultMod=samplePos;
+	float gridDist=0.0;
+	
 	if(lambda<lambdaMax)
 		{
 		samplePos+=dcDir*lambda;
@@ -68,6 +104,59 @@ void main()
 			{
 			/* Get the volume data value at the current sample position: */
 			vec4 vol=texture1D(colorMapSampler,texture3D(volumeSampler,samplePos).a);
+			
+			float largestDim = max(max(mcScale.x,mcScale.y),mcScale.z);
+			vec3 normScale = mcScale/largestDim;
+			float gradientStep = 0.002;			
+						
+			
+			/* Identify Grid or Checkerboard Region */
+			/* TODO: plug in uniform variables */
+			int temp_gridMode = 1;
+			float temp_gridSize = 10;
+			float temp_gridWidth = 0.5;
+			int temp_lightMode = 0;
+			
+			sampleMult = samplePos/mcScale;
+			
+			
+			/* Line Grid */
+			if (temp_gridMode == 1)
+				{
+				sampleMultMod = mod(sampleMult,temp_gridSize);
+				gridDist=min(min(sampleMultMod.x,sampleMultMod.y),sampleMultMod.z);
+				
+				if(gridDist < temp_gridWidth || (temp_gridSize - gridDist) < temp_gridWidth)
+					{
+						vol.rgb = vol.rgb * 0.5;
+					}
+				}
+			
+			/* Checkerboard Grid */
+			if (temp_gridMode == 2)
+				{
+				vec3 checkersRegion = floor(sampleMult/temp_gridSize);
+				float checkersColor = floor(mod(checkersRegion.x+checkersRegion.y+checkersRegion.z, 2.0));
+				
+				if(checkersColor == 0.0)
+					{
+						vol.rgb = vol.rgb * 0.75;
+					}
+				}
+			
+			if (vol.a > 0.05)
+				{				
+				float gradX = texture3D(volumeSampler,samplePos+(vec3(gradientStep,0,0)*normScale)).a - texture3D(volumeSampler,samplePos+(vec3(-gradientStep,0,0)*normScale)).a;
+				float gradY = texture3D(volumeSampler,samplePos+(vec3(0,gradientStep,0)*normScale)).a - texture3D(volumeSampler,samplePos+(vec3(0,-gradientStep,0)*normScale)).a;
+				float gradZ = texture3D(volumeSampler,samplePos+(vec3(0,0,gradientStep)*normScale)).a - texture3D(volumeSampler,samplePos+(vec3(0,0,-gradientStep)*normScale)).a;
+				vec3 gradient = -vec3(gradX, gradY, gradZ);
+				vec3 normGradient = normalize(gradient);
+				float magnitude = length(gradient);
+				//vec3 L = normalize(gl_LightSource[1].position.xyz - samplePos/mcScale);
+				vec3 L = normalize(mcDir);
+				vol.rgb = vol.rgb * blinn_phong(normGradient,normalize(mcDir),L,0);
+				}
+
 			
 			/* Accumulate color and opacity: */
 			accum+=vol*(1.0-accum.a);
